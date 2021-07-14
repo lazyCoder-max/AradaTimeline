f2e03151959315609079ae33bcf66b144db032d6
Danie Kidus <kidusdaniel@outlook.com>
2021-07-15 01:06:40 +0300

    Keyboard Shortcut for Marker Added

--- AradaTimeline/VideoStateAxisControl.cs
+++ AradaTimeline/VideoStateAxisControl.cs
@@ -258,6 +258,10 @@ namespace AradaTimeline
         {
             OnJoinButtonClicked();
         }
+        public void OnMarkerKeyPressed()
+        {
+            MarkerKeyPressed?.Invoke(this, new VideoEventArgs());
+        }
         protected virtual void OnJoinButtonClicked()
         {
             JoinButtonClick?.Invoke(this, new MarkerEventArgs() { Markers = Markers });
@@ -274,6 +278,12 @@ namespace AradaTimeline
             EventArgs = e;
             RefreshTimeline(AssignDrawer(e.Duration),e);
             DrawClip(e.Clips);
+            MarkerKeyPressed += VideoStateAxisControl_MarkerKeyPressed;
+        }
+
+        private void VideoStateAxisControl_MarkerKeyPressed(object sender, VideoEventArgs e)
+        {
+            DrawMarker(GetSeekMarkerPoint);
         }
 
         /// <summary>
@@ -859,7 +869,17 @@ namespace AradaTimeline
                 };
                 if (Markers.Count == 1)
                 {
-                    marker.IsStarting = false;
+                    if(marker.Time < Markers[0].Time)
+                    {
+                        marker.IsStarting = true;
+                        path.Data = Geometry.Parse("M18.088,16.035C12,16,14,22,12,24c-2-2,0-8-6.088-7.965c-0.516,0.003-0.913-0.471-0.913-0.987 c-0.001-2.54,0-9.459,0.001-12.05C5,2.445,5.448,2,6,2h12c0.552,0,1,0.445,1,0.997c0,2.591,0.001,9.51,0.001,12.05 C19.001,15.564,18.604,16.037,18.088,16.035z");
+                        Markers[0].IsStarting = false;
+                        var marker2 = (Path)_axisCanvasMarker.Children[0];
+                        marker2.Data = Geometry.Parse("M17,4c0,2.69,0.001,7.441,0.001,10.086C14.13,14.387,12.74,15.951,12,17.611c-0.74-1.66-2.13-3.224-5.001-3.525 C6.999,11.442,6.999,6.69,7,4H17 M18,2H6C5.448,2,5,2.445,5,2.997c0,2.591-0.001,9.51-0.001,12.05c0,0.515,0.394,0.987,0.908,0.987 c0.002,0,0.003,0,0.005,0c0.017,0,0.035,0,0.052,0c6.024,0,4.041,5.971,6.036,7.966c1.994-1.994,0.01-7.966,6.036-7.966 c0.017,0,0.035,0,0.052,0c0.002,0,0.003,0,0.005,0c0.514,0,0.908-0.472,0.908-0.987c0.001-2.54,0-9.459-0.001-12.05 C19,2.445,18.552,2,18,2L18,2z");
+                        Generic.StartingName = "Marker1";
+                    }
+                    else
+                        marker.IsStarting = false;
                 }
                 else
                 {
@@ -1120,6 +1140,7 @@ namespace AradaTimeline
         private event EventHandler<VideoEventArgs> VideoLoaded;
         public event EventHandler<MarkerEventArgs> JoinButtonClick;
         public event EventHandler<VideoEventArgs> TimePointMoved;
+        private event EventHandler<VideoEventArgs> MarkerKeyPressed;
         #endregion
     }
 
